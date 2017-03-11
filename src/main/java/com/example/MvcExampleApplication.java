a4461cfb1ee434028f340fc4c404953e2c317061
chandrakumar <saichandmar111@gmail.com>
2017-03-11 12:11:21 +0530

    This is new project

---
+++ src/main/java/com/example/MvcExampleApplication.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+@SpringBootApplication
+public class MvcExampleApplication {
+	@GetMapping("/")
+	public String he(){
+		return "hello";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcExampleApplication.class, args);
+	}
+}
