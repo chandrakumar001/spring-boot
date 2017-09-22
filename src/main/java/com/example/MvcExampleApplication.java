368cd3d1bd2f3cde9bfaad3bd287cbeea69bea90
Chandra Kumar <saichandmar111@gmail.com>
2017-09-22 23:19:48 +0530

    New files added

--- src/main/java/com/example/MvcExampleApplication.java
+++ src/main/java/com/example/MvcExampleApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MvcExampleApplication {
 	@GetMapping("/")
 	public String he(){
-		System.out.println("**Hello****");
+		System.out.println("**Hello One****");
 		return "hello";
 	}
 
